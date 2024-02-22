275e126b453bab50ecfc2f417ef73445cbd36b0e
barbarediasamidze910 <maridabarba16092@gmail.com>
2024-02-22 22:37:09 +0400

    second commit

---
+++ day 3/classwork.py
@@ -0,0 +1,30 @@
+print("hello lazare")
+print("hello gigachad")
+
+#გამხსნელი ფრჩხილი (
+#დამხურავი ფრჩხილი )
+# ()
+
+#გამხსნელი ბრჭყალი "
+#დამხურავი ბრჭყალი "
+#
+
+print("hello")
+print(10)
+print("barbare")
+print(10)
+ 
+
+print("gabriel")
+
+
+name="gabriel"
+
+print(name)
+
+
+item="bike"
+
+#item არის ცვლადის სახელი
+#bike არის ცვლადის მნიშვნელობა ხოლო = არის მინიჭებული ოპერატორი
+
