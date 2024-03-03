3d0941cf88d164c6158095a50263dc6743e4ea8c
barbarediasamidze910 <maridabarba16092@gmail.com>
2024-03-03 17:54:10 +0400

    day 4

---
+++ day 4/day4.py
@@ -0,0 +1,14 @@
+ # name = "nika"
+ # name = 'gabrieli'
+ # name = 'luka"
+
+# print ("my father's name is daviti")
+
+# variable1 = "nika"
+# variable2 = nika 10 1 2 3 4 5 5 1 2 4
+
+# print (10 + 10)
+
+print(10 * 5)
+
+# print(10 - 10)
