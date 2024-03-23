9b0dfa8ed010aba40ba550151126e4d8573ce355
barbarediasamidze910 <maridabarba16092@gmail.com>
2024-03-23 11:41:36 +0400

    day7

---
+++ day 7/day7HW.py
@@ -0,0 +1,8 @@
+a = type(int(input("enter your age : ")))
+print(a)
+
+b = type(str(input("enter your name : ")))
+print(b)
+
+c = type(float(input("enter a float : ")))
+print(c)
---
+++ day 7/day7HW2.py
@@ -0,0 +1,10 @@
+number1 = int(input("enter a number : "))
+
+
+number2 = int(input("enter a second number : "))
+
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
