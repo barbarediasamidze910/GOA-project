521485a990fdd859c14d22f3c15aaac1581df975
barbarediasamidze910 <maridabarba16092@gmail.com>
2024-04-20 12:38:02 +0400

    day11

---
+++ day11/day11HW1.py
@@ -0,0 +1,15 @@
+print(5>2)
+print(2>5)
+
+print(7<9)
+print(9<7)
+
+print(2<=2)
+print(4>=4)
+
+print(6==6)
+print(1==1)
+
+print(4!=2)
+print(6!=2)
+
---
+++ day11/day11HW3.py
@@ -0,0 +1,17 @@
+print(8==9)
+print(9==8)
+
+print(8>9)
+print(8<9)
+
+print(9>8)
+print(9<8)
+
+print(8>=9)
+print(8<=9)
+
+print(8>=9)
+print(8<=9)
+
+print(8!=9)
+print(9!=8)
