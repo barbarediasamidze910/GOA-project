731c9527ae7c08f7195cdc7b23734c963c12564f
barbarediasamidze910 <maridabarba16092@gmail.com>
2024-09-10 16:29:39 +0400

    day 19

---
+++ day 19/day 19HW1.py
@@ -0,0 +1,30 @@
+games = ['soccer', 'tic tac toe', 'snake', 'puzzle', 'rally']
+
+choice = int(input("enter which game you want to play: "))
+
+if choice == 1:
+    print(games[0])
+    print("is loading...")
+
+elif choice == 2:
+     print(games[1])
+     print("is loading...")
+
+elif choice == 3:
+     print(games[2])
+     print("is loading...")
+
+elif choice == 4:
+     print(games[3])
+     print("is loading...")
+
+elif choice == 5:
+     print(games[4])
+     print("is loading...")
+
+
+
+
+
+
+
---
+++ day 19/day 19HW2.py
@@ -0,0 +1,15 @@
+numbers = [ 1, 2 ,3 ,4 ,5, 6 ,7 ,8, 9 ]
+
+numbers[0] = 0
+
+numbers[1] = 1
+
+numbers[2] = 2
+
+numbers[3] = 3
+
+numbers[4] = 4
+
+numbers[5] = 5
+
+print(numbers)
