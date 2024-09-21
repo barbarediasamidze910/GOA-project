7c76066bdfc3317cd8a7ab22ff44097dc5513dc0
barbarediasamidze910 <maridabarba16092@gmail.com>
2024-09-21 12:22:03 +0400

    'day33'

---
+++ day 19/day 19HW3.py
@@ -0,0 +1,67 @@
+print(3>2)
+print(2<1)
+print(3==2)
+print(2!=3)
+
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+
+for i in range(1, 10):
+    print(i)
+
+for i in range(1, 10, 1):
+    print(i)
+
+number = int(input("enter a number from 1-5: "))
+
+if number == 1:
+    print("hi")
+
+elif number == 2:
+    print("hi")
+    print("hi")
+
+elif number == 3:
+    print("hi")
+    print("hi")
+    print("hi")
+
+elif number == 4:
+    print("hi")
+    print("hi")
+    print("hi")
+    print("hi")
+
+else:
+    print("hi")
+    print("hi")
+    print("hi")
+    print("hi")
+    print("hi")
+
+numberss = [2,1,3,5,4]
+
+num = int(input("enter a number: "))
+
+if num == 0:
+    print(numberss[0])
+
+elif num == 1:
+    print(numberss[1])
+
+elif num == 2:
+    print(numberss[2])
+
+elif num == 3:
+    print(numberss[3])
+
+elif num == 4:
+    print(numberss[4])
+
+elif num == 5:
+    print(numberss[5])
+
+
--- day 32/day 32CL6.py
+++ day 32/day 32CL6.py
@@ -5,11 +5,11 @@ def length():
     int(input("enter the length: "))
 
 def calculate_area():
-    print(width + length)
+    print(width + length * 2)
 
-def calculate_area2():
-    print(calculate_area + calculate_area)
+
+
 
 width()
 length()
-calculate_area2()
+calculate_area()
