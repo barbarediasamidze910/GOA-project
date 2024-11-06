a3fc210cf378d52b98391ece894dbf4e399ff54f
barbarediasamidze910 <maridabarba16092@gmail.com>
2024-11-06 16:12:44 +0400

    day 39

--- day 38/day30HW3.py
+++ day 38/day30HW3.py
@@ -4,5 +4,5 @@ def sum(x):
         res += i
         return res
     
-sum()
+
         
--- day 38/day38HW2.py
+++ day 38/day38HW2.py
@@ -1,12 +1,12 @@
 def calculate_average(numbers):
-    if len(numbers) == 0:  # Check if the list is empty
-        return 0  # Return 0 or handle empty list as needed
+    if len(numbers) == 0:  
+        return 0  
 
-    total = sum(numbers)  # Calculate the sum of the list
-    average = total / len(numbers)  # Calculate the average
-    return average  # Return the average value
+    total = sum(numbers)  
+    average = total / len(numbers)  
+    return average  
 
-# Example usage:
+
 numbers_list = [10, 20, 30, 40, 50]
 average_value = calculate_average(numbers_list)
 print(f"The average is: {average_value}")
