36d694b94a7592d7626f584c2099f02fc582a74d
barbare <maridabarbare1609@gmail.com>
2025-02-01 14:47:44 -0800

    day 51

---
+++ test python/prankANA.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+#width (10)
+#color ('red')
+#forward(200)
+#left(90)
+#right(90)
+#penup()
+#pendown()
+#beginfill()
+#endfill()
+
+
+width(10)
+
+forward(200)
+penup()
+left(90)
+forward(200)
+
+
+
